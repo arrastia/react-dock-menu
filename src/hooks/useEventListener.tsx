cb277acba2972fe5398da94756ecf93971c790c1
Arrastia <arrastia@pm.me>
2023-03-04 13:20:22 +0100

    ADD - useEventListener custom hook

---
+++ src/hooks/useEventListener.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+interface Props {
+  appendTo?: Window | Element;
+  event: keyof WindowEventMap;
+  handler: <T extends Event>(event: T) => void;
+  options?: AddEventListenerOptions;
+}
+
+export function useEventListener({ appendTo, event, handler, options }: Props) {
+  useEffect(() => {
+    const node = appendTo || document;
+
+    if (!handler || !node) return;
+
+    node.addEventListener(event, handler, options);
+
+    return () => {
+      node.removeEventListener(event, handler, options);
+    };
+  }, [appendTo, event, handler, options]);
+
+  return () => {
+    const node = document;
+    node?.removeEventListener(event, handler, options);
+  };
+}
